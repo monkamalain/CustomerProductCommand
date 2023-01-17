d1cf4fc00da5be567cda569d0aec4f25caf7d6bb
monkamalain <asmonkam@gmail.com>
2023-01-17 11:17:47 -0500

    Update file src/main/java/ca/tmas/cpc/controller/ProductController.java

--- src/main/java/ca/tmas/cpc/controller/ProductController.java
+++ src/main/java/ca/tmas/cpc/controller/ProductController.java
@@ -6,10 +6,8 @@ import ca.tmas.cpc.service.ProductService;
 import ca.tmas.cpc.utils.ProductStatus;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,16 +22,12 @@ import java.util.Set;
 @Setter
 public class ProductController {
 
-//    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ProductController.class);
-
     private ProductService productService;
 
-    private ModelMapper mapper;
 
     @RequestMapping(value = "/createProduct", method = RequestMethod.POST)
-    public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) {
+    synchronized public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) {
         try{
-//            log.info("Entity Created");
             return new ResponseEntity<ProductDto>(productService.createProduct(productDto), HttpStatus.CREATED);
         } catch(GlobalFailedException ex) {
             System.err.println(ex.getMessage());
@@ -42,9 +36,8 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getProductByRef/{ref}", method = RequestMethod.GET)
-    public ResponseEntity<Optional<ProductDto>> getProductByRef(@PathVariable(value = "ref", required = true) String ref) {
+    synchronized public ResponseEntity<Optional<ProductDto>> getProductByRef(@PathVariable(value = "ref", required = true) String ref) {
         if(productService.getProductByRef(ref).isPresent()) {
-//            log.info("Entity existed");
             return new ResponseEntity<Optional<ProductDto>>(productService.getProductByRef(ref), HttpStatus.OK);
         } else {
             return new ResponseEntity<Optional<ProductDto>>(Optional.empty(), HttpStatus.NOT_FOUND);
@@ -52,9 +45,8 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/patchProduct/{ref}", method = RequestMethod.PATCH)
-    public ResponseEntity<ProductDto> patiallyUpdateProduct(@PathVariable(value = "ref", required = true) String ref, @Valid @RequestBody ProductDto productDto) {
+    synchronized public ResponseEntity<ProductDto> patiallyUpdateProduct(@PathVariable(value = "ref", required = true) String ref, @Valid @RequestBody ProductDto productDto) {
         if((productService.getProductByRef(productDto.getRefProduct()).isPresent())) {
-//            log.info("Entity update patially");
             return new ResponseEntity<ProductDto>(productService.patiallyUpdateProduct(ref, productDto), HttpStatus.OK);
         } else {
             return new ResponseEntity<ProductDto>(HttpStatus.NOT_FOUND);
@@ -62,9 +54,8 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/putProduct/{ref}", method = RequestMethod.PUT)
-    public ResponseEntity<ProductDto> totalyUpdateProduct(@PathVariable(value = "ref", required = true) String ref, @Valid @RequestBody ProductDto productDto) {
+    synchronized public ResponseEntity<ProductDto> totalyUpdateProduct(@PathVariable(value = "ref", required = true) String ref, @Valid @RequestBody ProductDto productDto) {
         if((productService.getProductByRef(productDto.getRefProduct()).isPresent()) && (productDto.getStateProduct().equals(ProductStatus.UNAVAILABLE.name()))) {
-//            log.info("Entity update totally");
             return new ResponseEntity<ProductDto>(productService.totalyUpdateProduct(ref, productDto), HttpStatus.OK);
         } else {
             System.out.println("Je ne peux etre modifie");
@@ -73,9 +64,8 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/deleteProductByRef/{ref}", method = RequestMethod.DELETE)
-    public ResponseEntity<Optional<ProductDto>> deleteProductByRef(@PathVariable(value = "ref", required = true) String ref) {
+    synchronized public ResponseEntity<Optional<ProductDto>> deleteProductByRef(@PathVariable(value = "ref", required = true) String ref) {
         if(productService.deleteProductByRef(ref).isPresent()) {
-//            log.info("Entity Deleted");
             return new ResponseEntity<Optional<ProductDto>>(productService.deleteProductByRef(ref), HttpStatus.OK);
         } else {
             return new ResponseEntity<Optional<ProductDto>>(Optional.empty(), HttpStatus.NOT_FOUND);
@@ -83,7 +73,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getAllProductsAvailables/{state}", method = RequestMethod.GET)
-    public ResponseEntity<List<ProductDto>> getAllProductsAvailable(@PathVariable(value = "state", required = true) String state) {
+    synchronized public ResponseEntity<List<ProductDto>> getAllProductsAvailable(@PathVariable(value = "state", required = true) String state) {
         state = state.toUpperCase();
         if(state.equals(ProductStatus.AVAILABLE.name())) {
             return new ResponseEntity<List<ProductDto>>(productService.getAllProductsByStateAvailable(state), HttpStatus.OK);
@@ -93,7 +83,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getAllProductsUnavailables/{state}", method = RequestMethod.GET)
-    public ResponseEntity<List<ProductDto>> getAllProductsUnavailable(@PathVariable(value = "state", required = true) String state) {
+    synchronized public ResponseEntity<List<ProductDto>> getAllProductsUnavailable(@PathVariable(value = "state", required = true) String state) {
         state = state.toUpperCase();
         if(state.equals(ProductStatus.UNAVAILABLE.name())) {
             return new ResponseEntity<List<ProductDto>>(productService.getAllProductsByStateUnavailable(state), HttpStatus.OK);
@@ -103,7 +93,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getAllRefProduct", method = RequestMethod.GET)
-    public ResponseEntity<Set<String>> getAllRefProduct() {
+    synchronized public ResponseEntity<Set<String>> getAllRefProduct() {
         if(productService.getAllRefProduct().isEmpty()) {
             return new ResponseEntity<Set<String>>(Collections.emptySet(), HttpStatus.NOT_FOUND);
         } else {
@@ -112,7 +102,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getAllProduct", method = RequestMethod.GET)
-    public ResponseEntity<List<ProductDto>> getAllProduct() {
+    synchronized public ResponseEntity<List<ProductDto>> getAllProduct() {
         if(productService.getAllProduct().isEmpty()) {
             return new ResponseEntity<List<ProductDto>>(Collections.emptyList(), HttpStatus.NOT_FOUND);
         } else{
@@ -121,7 +111,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getProductByPrice/{price}", method = RequestMethod.GET)
-    public ResponseEntity<List<ProductDto>> getProductByPrice(@PathVariable(value = "price", required = true) double price) {
+    synchronized public ResponseEntity<List<ProductDto>> getProductByPrice(@PathVariable(value = "price", required = true) double price) {
         if(productService.getProductByPrice(price).isEmpty()) {
             return new ResponseEntity<List<ProductDto>>(Collections.emptyList(), HttpStatus.NOT_FOUND);
         } else{
@@ -130,7 +120,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getProductByPriceBetween/{price1}/{price2}", method = RequestMethod.GET)
-    public ResponseEntity<List<ProductDto>> getProductByPriceBetween(@PathVariable(value = "price1", required = true) double price1, @PathVariable(value = "price2", required = true) double price2) {
+    synchronized public ResponseEntity<List<ProductDto>> getProductByPriceBetween(@PathVariable(value = "price1", required = true) double price1, @PathVariable(value = "price2", required = true) double price2) {
         if((productService.getProductByPriceBetween(price1, price2).isEmpty()) && (price1 >= price2)){
             return new ResponseEntity<List<ProductDto>>(Collections.emptyList(), HttpStatus.NOT_FOUND);
         } else{
@@ -139,7 +129,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getProductByCreationDate/{creationDate}", method = RequestMethod.GET)
-    public ResponseEntity<List<ProductDto>> getProductByCreationDate(@PathVariable(value = "creationDate", required = true) String creationDate) {
+    synchronized public ResponseEntity<List<ProductDto>> getProductByCreationDate(@PathVariable(value = "creationDate", required = true) String creationDate) {
         if(productService.getProductByCreationDate(creationDate).isEmpty()){
             return new ResponseEntity<List<ProductDto>>(Collections.emptyList(), HttpStatus.NOT_FOUND);
         } else{
@@ -148,7 +138,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getProductBySaleDate/{saleDate}", method = RequestMethod.GET)
-    public ResponseEntity<List<ProductDto>> getProductBySaleDate(@PathVariable(value = "saleDate", required = true) String saleDate) {
+    synchronized public ResponseEntity<List<ProductDto>> getProductBySaleDate(@PathVariable(value = "saleDate", required = true) String saleDate) {
         if(productService.getProductBySaleDate(saleDate).isEmpty()){
             return new ResponseEntity<List<ProductDto>>(Collections.emptyList(), HttpStatus.NOT_FOUND);
         } else{
@@ -157,7 +147,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/getProductBySaleDateAndPrice/{saleDate1}/{saleDate2}", method = RequestMethod.GET)
-    public ResponseEntity<Double> getProductBySaleDateAndPrice(@PathVariable(value = "saleDate1", required = true) String saleDate1, @PathVariable(value = "saleDate2", required = true) String saleDate2) {
+    synchronized public ResponseEntity<Double> getProductBySaleDateAndPrice(@PathVariable(value = "saleDate1", required = true) String saleDate1, @PathVariable(value = "saleDate2", required = true) String saleDate2) {
         if(productService.getProductBySaleDateAndPrice(saleDate1, saleDate2) == 0.0d){
             return new ResponseEntity<Double>(0.0d, HttpStatus.NOT_FOUND);
         } else{
